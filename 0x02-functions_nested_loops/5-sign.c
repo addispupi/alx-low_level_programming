5d2deb3209d110e70fc1af5063909f9c10f04fe2
Addisu <pupiatpixar@gmail.com>
2023-07-18 14:08:55 -0700

    Tasks from 4 to 5 re-executed adding betty formats

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * print_sign - Determins the sign of integer.
+ * @n: the number
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ */
 int print_sign(int n)
 {
 	if (n > 0)
