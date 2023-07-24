114a76fe8ece78d3dc0229b065c87f5533e5b562
Addisu <pupiatpixar@gmail.com>
2023-07-24 13:58:50 -0700

    Tasks from 1 to 7 added

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - prints s string followed by a new line
+ * @str: the string
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
