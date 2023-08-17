941bc8e4e60077c52983766f888c90480d987e31
Addisu <pupiatpixar@gmail.com>
2023-08-17 13:01:33 -0700

    0 sum the mall : updated

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,28 +1,25 @@
-#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
 /**
- * sum_them_all - sum of variadic variable
- * @n:const
- * Return:int
- */
+* sum_them_all - sum of all its parameters
+* @n: n args
+* Return: sum or if n == 0, return 0
+*/
 int sum_them_all(const unsigned int n, ...)
 {
-unsigned int i;
-va_list list;
-double sum = 0;
+	int sum = 0;
+	unsigned int i;
+	va_list arguments;
 
-	if (n == 0)
+	if (n)
 	{
-		return (0);
+		va_start(arguments, n);
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(arguments, int);
+		}
+		va_end(arguments);
 	}
-	if (n == 0)
-	{
-		return (0);
-	}
-va_start(list, n);
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(list, int);
-	}
-	va_end(list);
 	return (sum);
 }
